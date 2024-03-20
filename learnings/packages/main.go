793b878762270cdcfd2ebb3cc83b388cd80e7b25
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-03-19 23:09:33 -0300

    Extern packages learning

--- learnings/packages/main.go
+++ learnings/packages/main.go
@@ -1,13 +1,18 @@
 package main
 
 //To declare a private function the first caracter must be lower, and if you want a public function declare with first caracter Upper
+// Cmd command go tidy remove all dependencies with no usage in your application
 
 import (
 	"fmt"
 	"modules/auxiliary"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Olá mundo!")
 	auxiliary.Write()
+	error := checkmail.ValidateFormat("caiogualbertodev@outlook.com")
+	fmt.Println(error)
 }
