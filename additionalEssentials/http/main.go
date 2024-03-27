ba7a96bdc60d5632b0df5c4163280fdea2d0c97b
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-03-27 16:47:13 -0300

    Http functions

---
+++ additionalEssentials/http/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Olá mundo!"))
+	})
+
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Olá usuários!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":5000", nil)) //go run main.go
+}
